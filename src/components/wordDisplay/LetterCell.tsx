4702377a7b806712c6da30f14651371c38580a10
Hubert Paprocki <hubertpaprocki.dev@gmail.com>
2024-04-18 12:00:37 +0200

    feat:add letterCell component button

--- src/components/wordDisplay/LetterCell.tsx
+++ src/components/wordDisplay/LetterCell.tsx
@@ -4,10 +4,15 @@ import classes from "./WordDisplay.module.scss";
 interface LetterCellProps {
   readonly letter: string;
   readonly visible?: boolean;
+  readonly button?: boolean;
 }
 
 function LetterCell(props: LetterCellProps) {
-  return (
+  return props.button ? (
+    <li>
+      <button className={classes.listItemButton}>{props.letter}</button>
+    </li>
+  ) : (
     <li className={classes.listItem}>
       {props.visible ? props.letter : <>&#x200b; &#x200b;</>}
     </li>
